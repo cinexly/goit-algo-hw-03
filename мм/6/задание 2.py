43b4f4ea33cf75203e61f7fe6075f9f30b6f1f82
cinexly <cinexly@gmail.com>
2026-04-29 23:03:48 +0300

    Delete мм/6 directory

--- мм/6/задание 2.py
+++
@@ -1,9 +0,0 @@
-import random
-
-
-def get_numbers_ticket(min, max, quantity):
-    if min < 1 or max > 1000 or not (min <= quantity <= max):
-        return []
-
-    numbers = random.sample(range(min, max + 1), quantity)
-    return sorted(numbers)
